3565a53a386ae8acd9d200ddb3ec32a7f7758a15
zhkzyth <zhkzyth@gmail.com>
2013-02-18 09:13:03 +0800

    test

---
+++ skel/tmpl.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+%FILENAME%
+
+Created by <zhkzyth@gmail.com> on %DATE%
+"""
+
+import numpy as np
+import pylab as pl
+
+
+def main():
+    %START%
+
+if __name__ == '__main__':
+    main()
